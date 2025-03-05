3018c7faa979673b6e68390cdebe59522dd67569
Bhoovan Kumar <bhoovan2310@gmail.com>
2025-03-05 20:32:09 +0530

    UC1 Done -> Employee Payroll CRUD Operation Using REST API

---
+++ src/main/java/com/spring/employeePayroll/model/Employee.java
@@ -0,0 +1,14 @@
+package com.spring.employeePayroll.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+}
